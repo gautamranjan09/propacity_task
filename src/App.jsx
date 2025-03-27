c9b5a955a6c99acb661a68fede166b6de884612e
gautamranjan09 <gautamranjan96@gmail.com>
2025-03-27 14:19:12 +0530

    fix: Correct import path for Categories component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
-import HomePage from "./Pages/HomePage";
-import Categories from "./pages/Categories";
+import HomePage from "./pages/HomePage";
+import Categories from "./pages/Categories.jsx";
 
 function App() {
   return (
